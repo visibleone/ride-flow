0fe27c49be9539a38c091e56615c8ad3bf82bce8
Alexander.Atanasov <visibleone@gmail.com>
2025-11-03 18:46:51 +0200

    [user-service] - Make keyckoak_id field unique

--- user-service/src/main/resources/db/migration/V1__create_users_table.sql
+++ user-service/src/main/resources/db/migration/V1__create_users_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users
 (
     id          UUID PRIMARY KEY,
-    keycloak_id UUID         NOT NULL, -- links to Keycloak identity
+    keycloak_id UUID         NOT NULL UNIQUE, -- links to Keycloak identity
     email       VARCHAR(255) NOT NULL UNIQUE,
     name        VARCHAR(255) NOT NULL,
     role        VARCHAR(20)  NOT NULL, -- 'RIDER' or 'DRIVER'
