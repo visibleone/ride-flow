684d1f1c47808176246c24a354b809cf5518310d
Alexander.Atanasov <visibleone@gmail.com>
2025-11-06 15:24:08 +0200

    [user-service] - Refactor test security configuration; add `@WithMockUser` for method-level security tests, remove custom test authentication filter. Update dependencies.

--- user-service/src/test/java/com/example/userservice/integration/UserServiceIntegrationTest.java
+++ user-service/src/test/java/com/example/userservice/integration/UserServiceIntegrationTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -24,7 +25,10 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @SpringBootTest
 @AutoConfigureMockMvc
 @Testcontainers
-@ActiveProfiles("test")
+@WithMockUser(
+    username = "test-user",
+    roles = {"driver"})
+ @ActiveProfiles("test")
 class UserServiceIntegrationTest {
   @Container @ServiceConnection
   static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16");
